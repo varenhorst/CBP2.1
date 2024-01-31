412a5f49cff2e57bd1169f82b28a23dae69e5f46
varenhorst <alexvarenhorst@gmail.com>
2024-01-31 01:04:07 -0600

    Fetch

--- content.js
+++ content.js
@@ -31,34 +31,38 @@ class YoutubeInstance{
         alert('test');
         this.videoElement = document.querySelector('.html5-video-container video');
         this.progressBar = document.querySelector('ytp-progress-bar');
-        this.fetchVideo();
+        this.fetchVideo().then((data)=>{
+            
+        }).catch(()=>{
+            console.warn('Can not initialze extension. Request to server failed.');
+        });
     }
 
+
+    //Get Content from video
     async fetchVideo(){
-        let xhr = new XMLHttpRequest();
+        return new Promise((resolve,reject)=>{
+            let xhr = new XMLHttpRequest();
 
-        // Configure it: GET-request for the URL
-        xhr.open('GET', 'https://catfact.ninja/fact', true);
-      
-        // Setup a callback function to handle the response
-        xhr.onreadystatechange = function() {
-          // Check if the request is complete (readyState 4) and if the status is OK (status 200)
-          if (xhr.readyState === 4 && xhr.status === 200) {
-            // Parse the JSON response
-            let response = JSON.parse(xhr.responseText);
-      
-            // Log the cat fact to the console (you can do whatever you want with the data here)
-            console.log('Cat Fact:', response.fact);
-            alert(response.fact);
-          }
-        };
-      
-        // Send the request
-        xhr.send();
+            xhr.open('GET', 'https://catfact.ninja/fact', true);
+          
+            xhr.onreadystatechange = function() {
+              if (xhr.readyState === 4 && xhr.status === 200) {
+                // Parse the JSON response
+                let response = JSON.parse(xhr.responseText);
+          
+                console.log('Cat Fact:', response.fact);
+                alert(response.fact);
+                resolve(response);
+              } else{
+                  reject(`Error : ${xhr.status}`);
+              }
+            };
+          
+            // Send the request
+            xhr.send();
+        });
     }
-
-    
-
 
 
 }
